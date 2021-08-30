1cf71652761f3893beae01e681000a5d8ae9fec1
temirlanmur <temirlanmurzagaliyev@gmail.com>
2021-08-30 21:35:18 +0600

    Add URL routes for the pages of application

--- auctions/urls.py
+++ auctions/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("user/<str:username>", views.user, name="user_page"),
+    path("category/<str:slug>", views.category, name="category"),
+    path("category", views.categories, name="categories"),
+    path("listing/<int:id>", views.listing, name="listing"),
+    path("create-listing", views.create_listing, name="create_listing"),
+    path("watchlist", views.watchlist, name="watchlist")
 ]
