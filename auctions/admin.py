e5ebac8e6ebdf3d83d947f4f9831fd19a147a55a
temirlanmur <temirlanmurzagaliyev@gmail.com>
2021-08-30 21:36:50 +0600

    Add all models for register in admin
    Models include: User, ListingCategory, Listing, Bid, Comment.

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, ListingCategory, Listing, Bid, Comment
+
+admin.site.register([User, ListingCategory, Listing, Bid, Comment])
