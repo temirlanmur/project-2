233faa09a64d6bdd04daac17d92ccbba7654a762
temirlanmur <temirlanmurzagaliyev@gmail.com>
2021-09-12 17:27:09 +0600

    Modify patterns to use CBVs instead of function views

--- auctions/urls.py
+++ auctions/urls.py
@@ -3,14 +3,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.Index.as_view(), name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("user/<str:nickname>", views.user, name="user_page"),
-    path("category/<str:slug>", views.category, name="category"),
-    path("category", views.categories, name="categories"),
-    path("listing/<int:id>", views.listing, name="listing"),
-    path("create-listing", views.create_listing, name="create_listing"),
-    path("watchlist", views.watchlist, name="watchlist")
+    path("user/<str:username>/edit", views.UserUpdate.as_view(), name="update_user"),
+    path("user/<str:username>", views.UserDetail.as_view(), name="user_page"),
+    path("category/create", views.CategoryCreate.as_view(), name="create_category"),
+    path("category/<str:slug>", views.CategoryDetail.as_view(), name="category"),
+    path("category", views.CategoryList.as_view(), name="categories"),
+    path("listing/<int:id>/edit", views.ListingUpdate.as_view(), name="update_listing"),
+    path("listing/<int:id>/close", views.ListingClose.as_view(), name="close_listing"),
+    path("listing/<int:id>", views.ListingDetail.as_view(), name="listing"),
+    path("listing/new", views.ListingCreate.as_view(), name="create_listing"),    
+    path("watchlist", views.WatchlistDetail.as_view(), name="watchlist")
 ]
