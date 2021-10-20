88243cdf0013e16090ee0e63d544794f92397e02
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2021-10-20 11:05:21 +0600

    Modify index view to inherit from Django's ListView
    Add pagination with custom page range. Put listing card into separate
    template.

--- auctions/urls.py
+++ auctions/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.Index.as_view(), name="index"),
+    path("", views.ListingList.as_view(), name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
--- auctions/views.py
+++ auctions/views.py
@@ -3,7 +3,7 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-from django.views.generic import View
+from django.views.generic import View, ListView
 from django.core.exceptions import PermissionDenied
 
 from .models import User, ListingCategory, Listing
@@ -11,15 +11,34 @@ from .forms import UserForm, ListingCategoryForm, ListingForm, CommentForm, BidF
 from .utils import ConstructListMixin
 
 
-class Index(ConstructListMixin, View):
-    model = Listing
+class ListingList(ListView):
+    context_object_name = "listings"
+    paginate_by = 6    
     template_name = "auctions/index.html"
 
-    def get(self, request):
-        active_listings = self.model.objects.filter(active=True)
-        return render(request, self.template_name, {
-            "construct_list": self.construct_list(active_listings)
-        })
+    pages_on_each_side = 1  # The number of pages on each side of the current page number
+
+    def get_queryset(self):
+        active_listings = Listing.objects.filter(active=True)
+        return active_listings
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Add custom page range to the context. (For pagination)
+        page_number = context['page_obj'].number
+        num_pages = context['paginator'].num_pages
+
+        left_index = int(page_number) - self.pages_on_each_side
+        if left_index < 1:
+            left_index = 1
+        right_index = int(page_number) + self.pages_on_each_side
+        if right_index > num_pages:
+            right_index = num_pages
+        custom_range = range(left_index, right_index + 1)  # Because python range(1, 4) is from 1 till 3
+        
+        context['custom_page_range'] = custom_range
+        return context
 
 
 def login_view(request):
